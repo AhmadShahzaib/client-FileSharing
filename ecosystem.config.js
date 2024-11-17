b75cdd0343da85641689bc29c560c44f7c6d858d
AhmadShahzaib <ahmadshahzib683@gmail.com>
2024-11-17 11:00:21 +0500

    added

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,13 @@ module.exports = {
       PM2_SERVE_PATH: './build',
       PM2_SERVE_PORT: 3000,
       PM2_SERVE_SPA: 'true',
-      PM2_SERVE_HOMEPAGE: '/index.html'
+      PM2_SERVE_HOMEPAGE: '/index.html',
+      PM2_SERVE_REWRITE_RULES: JSON.stringify([
+        { "from": "/login", "to": "/index.html" },
+        { "from": "/register", "to": "/index.html" },
+        { "from": "/dashboard", "to": "/index.html" },
+        { "from": "/share/*", "to": "/index.html" }
+      ])
     }
   }]
 }; 
