90cd151c277b09028e5c5c3295b1a30e058858ed
AhmadShahzaib <ahmadshahzib683@gmail.com>
2024-11-18 10:22:16 +0500

    added spa

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,8 @@ module.exports = {
     script: "serve",
     env: {
       PM2_SERVE_PATH: './build',
-      PM2_SERVE_PORT: 3000
+      PM2_SERVE_PORT: 3000,
+      PM2_SERVE_SPA: 'true'
     },
     env_production: {
       NODE_ENV: "production"
