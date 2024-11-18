07c03aa8be8850e60d3ba24f7f4af850548afbe7
AhmadShahzaib <ahmadshahzib683@gmail.com>
2024-11-18 10:45:14 +0500

    added config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,8 @@
 module.exports = {
   apps: [{
     name: "file-sharing-client",
-    script: "serve",
-    env: {
-      PM2_SERVE_PATH: './build',
-      PM2_SERVE_PORT: 3000,
-      PM2_SERVE_OPTS: '--spa'
-    },
+    script: "npx",
+    args: "serve -s build -l 3000",
     env_production: {
       NODE_ENV: "production"
     }
